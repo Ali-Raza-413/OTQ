5e58f749de642ebeebc94a50cf24127d5d7f783d
Ali-Raza-413 <razachali89@gmail.com>
2024-12-31 18:49:54 +0500

    fixed issues

--- src/app/sections/contactus.jsx
+++ src/app/sections/contactus.jsx
@@ -22,14 +22,14 @@ const Contactus = () => {
       <div className="relative z-10 text-center text-white max-w-2xl px-10 md:px-0">
         <div className="flex flex-col md:flex-row gap-5">
           <div>
-            <h1 className="text-4xl font-semibold mb-4">
+            <h1 className="md:text-4xl font-bold mb-4 text-2xl">
               Join hands with us on a success venture
             </h1>
             <p className="mb-6 text-lg font-[400]">
             Transfor your bidding processes and numbers with our experts.
             </p>
           </div>
-          <div className="md:mt-10">
+          <div className="md:mt-10 mb-5">
             <button className="bg-[#4d0362] text-white text-xl font-semibold w-[200px] h-[50px]  rounded-xl hover:bg-[#63078a] transition"
                     onClick={() => router.push("/contactus")}
             >
